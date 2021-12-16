97507b8c1b43f52a00cb3db6c54c6cd1159f7a8b
Berna <Berna@BERNA>
2021-12-16 17:19:26 +0300

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerScore = 0;
+            int computerScore = 0;
+            string exitKey = "";
+            while (exitKey != "e")
+            {
+                Console.Clear();
+                Console.WriteLine("Rock - Paper - Scissors");
+                Console.WriteLine();
+                Console.WriteLine("Choose [r]ock, [p]aper, [s]cissors or [e].");
+            GetInput:
+                string playerMove = "";
+                switch (Console.ReadLine().ToLower())
+                {
+                    case ("r"):
+                        playerMove = "rock";
+                        break;
+                    case ("p"):
+                        playerMove = "paper";
+                        break;
+                    case ("s"):
+                        playerMove = "scissors";
+                        break;
+                    case ("e"):
+                        exitKey = "e";//atama yapmasamda olur
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.. Try again..");
+                        goto GetInput;
+
+                }
+                Console.WriteLine($"Your choose {playerMove}");
+                Random rnd = new Random();
+                int random = rnd.Next(3);
+                string computerMove = "";
+                switch (random)
+                {
+                    case 0:
+                        computerMove = "rock";
+                        break;
+                    case 1:
+                        computerMove = "paper";
+                        break;
+                    case 2:
+                        computerMove = "scissors";
+                        break;
+                        
+                }
+               
+                if ((playerMove == "rock" && computerMove == "paper")||(playerMove == "paper" && computerMove == "scissors")||(playerMove == "scissors" && computerMove == "rock"))
+                {
+                    computerScore++;
+                    Console.WriteLine("You lose.Computer wins!");
+
+                }
+                else if ((playerMove == "rock" && computerMove == "scissors")||(playerMove == "paper" && computerMove == " rock")||(playerMove == "scissors" && computerMove == "paper"))
+                {
+                    playerScore++;
+                    Console.WriteLine("Computer lose.You win!");
+                }
+                else
+                {
+                    Console.WriteLine("This game was a draw.");
+                }
+                Console.WriteLine($"The computer choose {computerMove}");
+                Console.ReadKey();
+            }
+           
+
+        }
+    }  }
+
